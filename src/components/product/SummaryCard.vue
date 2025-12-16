e06adb02e68957d225951b2b3ff902a440577a18
huni2 <powerhch@gmail.com>
2025-12-17 02:16:15 +0900

    feat: #4 AS/정기점검 목록 페이지 구현

---
+++ src/components/product/SummaryCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-card>
+        <div class="card">
+        <span class="title">{{ title }}</span>
+        <h2>{{ value }}건</h2>
+        <small>{{ sub }}</small>
+        </div>
+    </el-card>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    value: Number,
+    sub: String,
+    type: String
+})
+</script>
+
+<style scoped>
+.card {
+    text-align: center;
+}
+.title {
+    color: #666;
+}
+</style>
