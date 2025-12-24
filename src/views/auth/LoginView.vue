37a94aa7ae8c7242687a2b2105643ca0baeba3e5
Kjandgo <rptmffld0204@gmail.com>
2025-12-24 15:01:46 +0900

    일부 오류 수정

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -38,9 +38,16 @@
           <span>로그인 상태 유지</span>
         </div> -->
 
-        <el-button type="primary" class="login-btn" :loading="loading" :disabled="loading" @click="login" size="large">
-          로그인
-        </el-button>
+        <el-button
+  type="primary"
+  class="login-btn"
+  :loading="loading"
+  :disabled="loading"
+  @click="login"
+  size="large"
+>
+  로그인
+</el-button>
       </form>
     </div>
 
