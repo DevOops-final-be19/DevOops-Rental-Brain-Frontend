2980aa79f77ebed71d9492a76ed52c01bad76bdb
Kjandgo <rptmffld0204@gmail.com>
2025-12-31 17:17:48 +0900

    api

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: import.meta.env.PROD
-        ? 'https://rentalbrain.shop'
+        ? '/api'
         : 'http://localhost:5000',
     withCredentials: true,
     headers: {
